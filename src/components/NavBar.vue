8abc569cb5138629802aab69fbdd980114b36b3c
charuzain <charuabhizain@gmail.com>
2022-05-28 08:28:00 -0400

    Add navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,20 @@
 <template>
-  This is navbar
+<nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#007500">
+  <div class="container-fluid navbar-nav">
+  <div>
+  <router-link to="/" class="navbar-brand ms-3">
+  <i class="bi bi-person-lines-fill me-2"></i>Contact Manager</router-link>
+   <router-link to="/contacts/add" class="navbar-brand ms-3">
+  <i class="bi bi-plus-circle-fill me-2"></i>Add Contact</router-link>
+   <router-link to="/contacts/edit/:id" class="navbar-brand ms-3">
+  <i class="bi bi-pen-fill me-2"></i>Edit Contact</router-link>
+  </div>
+      <form class="d-flex ">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-success" type="submit">Search</button>
+      </form>
+  </div>
+</nav>
 </template>
 
 <script>
