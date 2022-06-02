6960842e4d963f6a484712f634bf81ffaa3f6478
charuzain <charuabhizain@gmail.com>
2022-06-01 21:01:28 -0400

    Add contact page

--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -1,28 +1,40 @@
 <template>
- <div class="container mt-5">
-  <div class="row">
-    <div class="col-6">
-     <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting
-         text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <div class="row align-items-center">
+              <div class="col-sm-4">
+                <img
+                  src="https://png.pngitem.com/pimgs/s/130-1300380_female-user-image-icon-hd-png-download.png"
+                  alt="user image"
+                  class="user-img"
+                />
+              </div>
+              <div class="col-sm-7">
+                <ul class="list-group">
+                  <li class="list-group-item">Name: </li>
+                  <li class="list-group-item">Email Address: </li>
+                  <li class="list-group-item">Contact No: </li>
+                </ul>
+              </div>
+              <div class="col-sm-1">
+                <router-link to="/contacts/edit/:Id" class="btn btn-primary my-1">
+                <i class="bi bi-pen-fill"></i>
+                </router-link>
+                <router-link to="/contacts/view/:Id" class="btn btn-warning my-1">
+                  <i class="bi bi-eye-fill"></i>
+                </router-link>
+                <i class="bi bi-trash-fill btn btn-danger my-1"></i>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
-    </div>
-    <div class="col-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting
-        text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-    </div>
-    </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -30,6 +42,9 @@ export default {
   name: 'ContactManager',
 };
 </script>
-
 <style>
+.user-img {
+  width: 150px;
+  border-radius: 50%;
+}
 </style>
